f28651bc9f7781286f3a191d6f6c7cbd3a5d7dcb
Abby <arolling@gmail.com>
2016-01-25 20:35:21 -0800

    Add address object constructor

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,18 @@
 function Contact(firstName,lastName) {
-  this.firstName = firstName,
-  this.lastName = lastName,
-  this.addresses = []
+  this.firstName = firstName;
+  this.lastName = lastName;
+  this.addresses = [];
 }
 
 Contact.prototype.fullName = function() {
   return this.firstName + " " + this.lastName;
 }
 
+function Address(street, city, state) {
+  this.street = street;
+  this.city = city;
+  this.state = state;
+}
 $(document).ready(function() {
   $("form#new-contact").submit(function(event) {
     event.preventDefault();
